fef72be7f7b9b5733348a1b2f3438b0048a64cc8
Alessandro Gario <alessandro.gario@gmail.com>
2022-04-16 16:52:38 +0200

    ebpf: Refactor the LLVMBridge class

--- ebpf/include/tob/ebpf/illvmbridge.h
+++ ebpf/include/tob/ebpf/illvmbridge.h
@@ -17,6 +17,7 @@ using namespace btfparse;
 
 namespace tob::ebpf {
 
+/// LLVMBridge error codes
 enum class LLVMBridgeErrorCode {
   Unknown,
   MemoryAllocationFailure,
@@ -35,30 +36,44 @@ enum class LLVMBridgeErrorCode {
   BitfieldError,
 };
 
+/// LLVMBridgeErrorCode printer
 struct LLVMBridgeErrorCodePrinter final {
   std::string operator()(const LLVMBridgeErrorCode &error_code) const;
 };
 
+/// LLVMBridge errors
 using LLVMBridgeError =
     btfparse::Error<LLVMBridgeErrorCode, LLVMBridgeErrorCodePrinter>;
 
+/// A BTF to LLVM bridge
 class ILLVMBridge {
 public:
+  /// A pointer to an LLVMBridge object
   using Ptr = std::unique_ptr<ILLVMBridge>;
+
+  /// Creates a new LLVM bridge instance, importing the given BTF types
   static Result<Ptr, LLVMBridgeError> create(llvm::Module &module,
                                              const IBTF &btf);
 
+  /// Returns the LLVM type for the specified type name
   virtual btfparse::Result<llvm::Type *, LLVMBridgeError>
   getType(const std::string &name) const = 0;
 
+  /// Reads the `path` value from `src` into `dest`
   virtual std::optional<LLVMBridgeError>
   read(llvm::IRBuilder<> &builder, llvm::Value *dest, llvm::Value *src,
        const std::string &path, llvm::BasicBlock *read_failed_bb) const = 0;
 
+  /// Constructor
   ILLVMBridge() = default;
+
+  /// Destructor
   virtual ~ILLVMBridge() = default;
 
+  /// Copy constructor (disabled)
   ILLVMBridge(const ILLVMBridge &) = delete;
+
+  /// Copy assignment operator (disabled)
   ILLVMBridge &operator=(const ILLVMBridge &) = delete;
 };
 
